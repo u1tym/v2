8b74660cfa374bf3a4c90f141cc8b6ccfd6a7098
yuichi toyama <yuichi-toyama@ase.co.jp>
2026-04-24 18:07:02 +0900

    動くかたちに

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ warnings.filterwarnings( 'ignore' )
 import tkinter as tk
 from ffpyplayer.player import MediaPlayer
 from ffpyplayer.pic import SWScale
-from PIL import Image, ImageTk
+from PIL import Image as PILImage, ImageTk
 import time
 
 def main() -> None:
@@ -30,57 +30,60 @@ def main() -> None:
     player: MediaPlayer = MediaPlayer("hoge.mp4")
 
     img_on_cnv = None
-    cnv_size = (640, 480)
     sws_to_rgb = None
     sws_src_fmt = ""
     sws_src_size = (0, 0)
 
     while True:
         frame, val = player.get_frame()
-        #print(val)
+
         if val == "eof":
             break
+
         if frame is None:
-            # フレーム未準備時もイベントを処理してウィンドウを固めない
-            root.update_idletasks()
-            root.update()
-            continue
-
-        img, video_pts = frame
-
-        # 表示
-
-        src_size = img.get_size()
-        src_fmt = img.get_pixel_format()
-        if src_fmt != "rgb24":
-            # PILで扱いやすいRGBに揃える
-            if sws_to_rgb is None or sws_src_fmt != src_fmt or sws_src_size != src_size:
-                sws_to_rgb = SWScale(src_size[0], src_size[1], src_fmt, ofmt="rgb24")
-                sws_src_fmt = src_fmt
-                sws_src_size = src_size
-            img = sws_to_rgb.scale(img)
-            src_size = img.get_size()
-
-        plane = img.to_bytearray()[0]
-        pil_img = Image.frombytes("RGB", src_size, bytes(plane))
-
-        tk_img = ImageTk.PhotoImage(image=pil_img)
-        cnv.image = tk_img  # 参照を保持してGCで消えないようにする
-
-        w, h = pil_img.size
-
-        audio_pts = player.get_pts()
-        if video_pts > audio_pts:
-            print(video_pts - audio_pts)
-            time.sleep(video_pts - audio_pts + 0.015)
-
-        if img_on_cnv is None:
-            setSize(root, w, h)
-            cnv.config(width=w, height=h)
-            img_on_cnv = cnv.create_image(0, 0, anchor='nw', image=tk_img)
+            print("")
         else:
-            cnv.itemconfig(img_on_cnv, image=tk_img)
-        
+            data, frame_pts = frame
+            # ffpyplayer の映像フレーム(Image)のみ処理する
+            # 音声フレームはこれらのメソッドを持たないため除外できる
+            if all(hasattr(data, attr) for attr in ("get_size", "get_pixel_format", "to_bytearray")):
+                # 映像
+                img = data
+                video_pts = frame_pts
+
+                # 表示
+
+                src_size = img.get_size()
+                src_fmt = img.get_pixel_format()
+                if src_fmt != "rgb24":
+                    # PILで扱いやすいRGBに揃える
+                    if sws_to_rgb is None or sws_src_fmt != src_fmt or sws_src_size != src_size:
+                        sws_to_rgb = SWScale(src_size[0], src_size[1], src_fmt, ofmt="rgb24")
+                        sws_src_fmt = src_fmt
+                        sws_src_size = src_size
+                    img = sws_to_rgb.scale(img)
+                    src_size = img.get_size()
+
+                plane = img.to_bytearray()[0]
+                pil_img = PILImage.frombytes("RGB", src_size, bytes(plane))
+
+                tk_img = ImageTk.PhotoImage(image=pil_img)
+                cnv.image = tk_img  # 参照を保持してGCで消えないようにする
+
+                w, h = pil_img.size
+
+                audio_pts = player.get_pts()
+                if video_pts > audio_pts:
+                    print(video_pts - audio_pts)
+                    time.sleep(video_pts - audio_pts + 0.015)
+
+                if img_on_cnv is None:
+                    setSize(root, w, h)
+                    cnv.config(width=w, height=h)
+                    img_on_cnv = cnv.create_image(0, 0, anchor='nw', image=tk_img)
+                else:
+                    cnv.itemconfig(img_on_cnv, image=tk_img)
+
         root.update_idletasks()
         root.update()
 
--- typings/ffpyplayer/player.pyi
+++ typings/ffpyplayer/player.pyi
@@ -2,17 +2,19 @@ from typing import Self
 from typing import Tuple
 from typing import Optional
 from typing import Union
-from typing import Literal
-import numpy as np
-from numpy.typing import NDArray
 
-VideoFrame = Tuple[NDArray[np.uint8], float]
+from ffpyplayer.pic import Image
+#import numpy as np
+#from numpy.typing import NDArray
+
+#VideoFrame = Tuple[NDArray[np.uint8], float]
+VideoFrame = Tuple[Image, float]
 AudioFrame = Tuple[bytes, float]
 
 FrameType = Optional[Union[VideoFrame, AudioFrame]]
-ValType = Optional[Literal["video", "audio", "eof"]]
+ValType = Optional[float]
 
 class MediaPlayer:
     def __init__(self: Self, filename: str) -> None: ...
-    def get_frame(self: Self) -> Union[Tuple[VideoFrame, Literal["video"]], Tuple[AudioFrame, Literal["audio"]], Tuple[None, Literal["eof"]]]:...
+    def get_frame(self: Self) -> Tuple[FrameType, ValType]:...
     def get_pts(self: Self) -> float: ...
